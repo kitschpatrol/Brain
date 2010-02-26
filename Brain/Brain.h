ce33775f6b5a95469650dde9563c77914982e50e
kitschpatrol <ermika@gmail.com>
2010-02-26 16:57:08 -0500

    Major rework into half-duplex hardware serial approach. Moved EEG power bands into an array for convenience. Added getters and setters.

--- Brain/Brain.h
+++ Brain/Brain.h
@@ -6,49 +6,34 @@
 #define Brain_h
 
 #include "WProgram.h"
-#include "NewSoftSerial.h"
 
 #define MAX_PACKET_LENGTH 32
+#define EEG_POWER_BANDS 8
 
 class Brain {
 	public:
-	  // ??? Any way to instantiate the NewSoftSerial in the library itself?
-	  // Use this constructor if you want to use the brain values internally,
-	  // or if you want to write your own serial output code.
-		Brain(NewSoftSerial &_brainSerial);
-		
-		// Use this constructor if you want automatic CSV serial out
-		Brain(NewSoftSerial &_brainSerial, Print &_printer);		
-		
-		// ??? Any way to loop this asynchronously in the library without wasting a timer?
-		// Put this in the loop() of your sketch.
+	  // TK Support for soft serials
+		Brain(HardwareSerial &_brainSerial);	
     void update();
-    
     boolean sendCSV;
     boolean noisyErrors;
     boolean debug;
-    
-    // ??? Better off using getters for these?
-    // ??? Better off using the inttypes?
-    // ??? I just need unsigned 8 bit ints.
-    byte signalQuality;
-    byte attention;
-    byte meditation;
-    
-    // ??? Is there a 24 bit unsigned integer type?
-    // ??? Each of these is wasting a byte.
-    unsigned long delta;
-    unsigned long theta;
-    unsigned long lowAlpha;
-    unsigned long highAlpha;
-    unsigned long lowBeta;
-    unsigned long highBeta;
-    unsigned long lowGamma;
-    unsigned long midGamma;
-		
+
+    byte getSignalQuality();    
+    byte getAttention();
+    byte getMediation();
+    unsigned long* getPowerArray();
+    unsigned long getDelta();
+    unsigned long getTheta();
+    unsigned long getLowAlpha();
+    unsigned long getHighAlpha();
+    unsigned long getLowBeta();
+    unsigned long getHighBeta();
+    unsigned long getLowGamma();
+    unsigned long getMidGamma();
+
   private:
-    NewSoftSerial* brainSerial;
-    Print* printer;
+    HardwareSerial* brainSerial;    
     byte packetData[MAX_PACKET_LENGTH];
     boolean inPacket;
     byte latestByte;
@@ -60,11 +45,21 @@ class Brain {
     byte eegPowerLength;
     boolean hasPower;
     void clearPacket();
+    void clearEegPower();
     boolean parsePacket();
     void printPacket();
     void init();
     void printCSV(); // maybe should be public?    
     void printDebug();
+
+    byte signalQuality;
+    byte attention;
+    byte meditation;
+    
+    // Lighter to just make this public, instead of using the getter?
+    unsigned long eegPower[EEG_POWER_BANDS];
+    
+
 
 
 };
--- Brain/Examples/Brain_Example/Brain_Example.pde
+++ Brain/Examples/Brain_Example/Brain_Example.pde
@@ -1,29 +1,31 @@
-// Brain Lib Example
+// Serial out Brain Lib Example
 
-// You can access 
-#include<NewSoftSerial.h>
+// If you want to send brain data out over serial (to a PC, for example),
+// give the Brain class constructor a second argument with the serial
+// object you want to use for output. Be sure to call update() in the main
+// loop.
+
 #include <Brain.h>
 
-// Set up the software serial to
-// listen for brain serial data on pin 2.
-NewSoftSerial brainSerial(2, 3);
-
-// Set up the brain parser to listen on the
-// software serial.
-Brain brain(brainSerial);
+// Set up the brain parser. It listens on the 
+// first argument, and sends output to thee second object.
+Brain brain(Serial);
 
 void setup() {
+  // Why do I have to call this again here, when it's already called
+  // in brain's init? If I don't, I get very strange behavior... printing to the serial
+  // works in the init function, but update() never seems to get called.
+  Serial.begin(9600);
+  
+ // Send detailed packet info.
+  brain.sendCSV = true;
+  brain.debug = true;
+  brain.noisyErrors = false; 
+  
 }
 
 void loop() {
-  // Grabs the brain data. Expect updates about once per second.
+  // Grabs the brain data and sends CSV out over the hardware serial.
+  // Expect packets about once per second.
   brain.update();
-  
-  // Here's the attention value. Runs from 0 - 100.
-  brain.attention
-  
-  // (TK do something with it)
-  
-  // See the documentation or the library source for a full list
-  // of brain variables.
 }
